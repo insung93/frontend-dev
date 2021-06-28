9c946d2b63572f8a52285a33cfc918d24c67b72e
insung93 <jis5780@gmail.com>
2021-06-28 12:15:57 +0900

    javascript 연습#2

---
+++ javascript-practices/ex05.js
@@ -0,0 +1,20 @@
+/*
+
+    구문(Statement)
+    1. 자바스크립트의 실행 단위
+    2. 구성 요소
+        값, 연산자 : 표현식
+        주석 : 구문을 구성해도 실행되지 않는다.
+        예약어 : if, else, for, ...
+    3. 구문의 예
+        if, if-else, if-elseif-else
+        while, do-while, for
+        switch
+        for-in
+        with(deprecated)
+        변수 정의(함수 정의)
+    4. 공백 : 토큰 구분, 무시
+    
+
+*/
+
