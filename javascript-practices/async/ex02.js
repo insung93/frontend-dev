b793e03f69689d08413813f8cd4fcbb4e7d434be
insung93 <jis5780@gmail.com>
2021-07-19 14:51:29 +0900

    비동기 테스트 #2

---
+++ javascript-practices/async/ex02.js
@@ -0,0 +1,28 @@
+const ex02 = function (param) {
+    return new Promise(function (resolve, reject) {
+        setTimeout(function () {
+            if (param == 'param-data') {
+                resolve('ok');
+            } else {
+                reject(new Error('fail'));
+            }
+        }, 1000);
+    })
+}
+
+
+if (require.main == module) {
+    // test01 : success
+    ex02("param-data").then(function (res) {
+        console.log(res);
+    });
+
+    // test02 : fail
+    ex02("param-error").catch(function (err) {
+        console.error(err.message);
+    });
+
+    console.log("waits....");
+} else {
+    module.exports = ex02;
+}
